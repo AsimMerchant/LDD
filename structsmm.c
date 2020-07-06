7ba18611f4ae7396eeef468d1fec130028bfbce7
AsimMerchant <asimthatsme@gmail.com>
2020-07-06 17:58:45 +0530

    Addresses showing properly

--- structsmm.c
+++ structsmm.c
@@ -7,18 +7,24 @@ static int entry(void)
 	
 	pr_alert("Size of struct page %ld\n",sizeof(struct page));
 
+	pr_alert("Size of a page=%u\n",PAGE_SIZE);
+
+	pr_alert("Page shift=%u\n",PAGE_SHIFT);
+
+	pr_alert("Page mask=%x\n",PAGE_MASK);
+
 	pr_alert("Number of physical pages=%ld\n",get_num_physpages());
 
 	ptr=kmalloc(100,GFP_KERNEL);
 
-	pr_alert("Logical addr=%p\n",ptr);
+	pr_alert("Logical addr=%lx\n",ptr);
 	
-	pr_alert("PAGE_OFFSET=%p\n",PAGE_OFFSET);
+	pr_alert("PAGE_OFFSET=%lx\n",PAGE_OFFSET);
 
-	pr_alert("ptr-PAGE_OFFSET=%p\n",ptr-PAGE_OFFSET);
+	pr_alert("ptr-PAGE_OFFSET=%lx\n",ptr-PAGE_OFFSET);
 
 	pa=(unsigned char *)__pa(ptr);
-	pr_alert("Physical address=%p\n",pa);
+	pr_alert("Physical address=%lx\n",pa);
 	return 0;
 }
 
